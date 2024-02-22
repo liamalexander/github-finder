7da3ced7272e00f2470cb06ef804a97750c5328b
Liam Alexander <liamalexander7@hotmail.co.uk>
2024-02-22 12:18:23 +0100

    add search

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -11,7 +11,7 @@ function UserResults() {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4
       lg:grid-cols-3 md:grid-cols-2'>
-        {users.map((user) => {
+        {users && users.map((user) => {
           return (
             <UserItem 
               key={user.id} 
