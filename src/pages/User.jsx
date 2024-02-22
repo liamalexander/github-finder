6ed45020c09658440b1e4762f054305b952b1d55
Liam Alexander <liamalexander7@hotmail.co.uk>
2024-02-22 14:24:27 +0100

    add user

---
+++ src/pages/User.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useEffect, useContext } from 'react'
+import GithubContext from '../context/github/GithubContext'
+
+function User() {
+  const { getUser, user } = useContext(GithubContext)
+
+  const params = useParams()
+
+  useEffect(() => {
+    getUser(params.login)
+    // getUserRepos(params.login)
+  }, [])
+  return <div>{user.login}</div>
+}
+
+export default User
